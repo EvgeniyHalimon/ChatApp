741156f4df39ead05125c5cbddd40b4c97d404c3
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-01-30 18:48:59 +0200

    feat: initialize of project

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useEffect, useRef } from "react";
+
+export default function Home() {
+    return(
+        <>
+            Home
+        </>
+    )
+}
