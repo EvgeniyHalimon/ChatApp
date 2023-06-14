7b097d9bc688b558787f94f06e200e06c66cf830
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-06-14 19:18:44 +0300

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ const httpServer = createServer(app)
 
 const io = new Server(httpServer, {
     cors: {
-        origin: "*",
+        origin: ["*", "https://chat-app-server-hvhnxg1vc-evgeniyhalimon.vercel.app/"],
         credentials: true
     }
 });
